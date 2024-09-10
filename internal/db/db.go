6e3a9f312f589ade2b79541111416ff46aa96f3d
imjenal <jyotsna17gupta@gmail.com>
2024-09-10 17:36:42 +0530

    connstring fix

--- internal/db/db.go
+++ internal/db/db.go
@@ -4,11 +4,14 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 func NewPostgresDB() (*sql.DB, error) {
 	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
